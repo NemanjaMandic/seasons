6d542667f5bc8e6bcafa2e3a677e9b6df9920b0e
NemanjaMandic <nemanja.mandic@folkmatic.com>
2018-12-13 17:18:42 +0100

    added class

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-  return <div>Hi there!</div>;
-};
+
+
+class App extends React.Component{
+
+  render(){
+    navigator.geolocation.getCurrentPosition(
+      (position) => console.log(position),
+      (err) => console.log(err)
+    );
+    return <div>Latitude: </div>;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
